e1e6a750b059649a2d1e93ee304ff907e28aaff6
oladee <deemajor230600@gmail.com>
2023-06-03 19:07:32 +0100

    included error msgs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   let useins = usein.value
   let selected = prefer.value
-  console.log(token)
+
       search.onclick = function(){
         cardList.innerHTML = ""
         fetch(
@@ -17,33 +17,43 @@ form.addEventListener("submit", (e) => {
         )
           .then((res) => res.json())
           .then((data) => {
+            console.log(data)
             let result = data.hits;
             console.log(result);
-            for(var i = 0; i < result.length; i++){
-              cardList.innerHTML += `
-              <div class='card' >
-              <img class='card-image' src=${result[i].recipe.image}>
-               <h1>${result[i].recipe.label}</h1>
-               <p id='summary'>Summary: ${result[i].recipe.healthLabels[0]}, ${result[i].recipe.healthLabels[1]}</p>
-               <div id='more-info' class='more-info'>
-               <p>${result[i].recipe.mealType}</p>
-                <p>${result[i].recipe.dishType}</p>
-                <p> ${result[i].recipe.ingredientLines.map(x => x)}</p>
+            if(result.length < 1){
+              cardList.innerHTML = `
+              <div class='error'>
+              <p>Looks like your entry is new to us!🤔🤔</p>
+              <p>please check your entry and try again!</p>
+              </div>
+              `
+            }else{
+              for(var i = 0; i < result.length; i++){
+                cardList.innerHTML += `
+                <div class='card' >
+                <img class='card-image' src=${result[i].recipe.image}>
+                 <h1>${result[i].recipe.label}</h1>
+                 <p id='summary'>Summary: ${result[i].recipe.healthLabels[0]}, ${result[i].recipe.healthLabels[1]}</p>
+                 <div id='more-info' class='more-info'>
+                 <p>${result[i].recipe.mealType}</p>
+                  <p>${result[i].recipe.dishType}</p>
+                  <p> ${result[i].recipe.ingredientLines.map(x => x)}</p>
+                 </div>
                </div>
-             </div>
-         `;
-         
-            }
-            let details = document.getElementsByClassName('more-info')
-            document.querySelectorAll('.card').forEach((x,index) => {
-              x.addEventListener('click', ()=>{
-                if(details[index].style.display == 'block'){
-                  details[index].style.display = 'none'
-                }else{
-                  details[index].style.display = 'block'
-                }
+           `;
+           
+              }
+              let details = document.getElementsByClassName('more-info')
+              document.querySelectorAll('.card').forEach((x,index) => {
+                x.addEventListener('click', ()=>{
+                  if(details[index].style.display == 'block'){
+                    details[index].style.display = 'none'
+                  }else{
+                    details[index].style.display = 'block'
+                  }
+                })
               })
-            })
+            }
           })
       }
       
